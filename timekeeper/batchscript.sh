1a7ac0c372e2d3305469c1d7f48443a74fc7de72
PreetiUdas <preetiudas@yahoo.com>
2018-12-05 12:36:25 +0000

    added script

--- timekeeper/batchscript.sh
+++ timekeeper/batchscript.sh
@@ -1,8 +1,35 @@
 #!/Users/preetiudas/.pyenv/shims/python3.7
-timekeeper/duplicates.py &&
-timekeeper/zeroesandones.py
 
-#chmod 755 timekeeper/batchscript.sh
-# 7 = read/write/ ex, 5 = read/ex
 # type python3.7
-#will give the path for shebang
+# will give the path for shebang
+
+# chmod 755 timekeeper/batchscript.sh
+# chmod 755 timekeeper/bubblesort.py
+# chmod 755 timekeeper/duplicates.py
+# chmod 755 timekeeper/mechacoach.py
+# chmod 755 timekeeper/mostfrequentwords.py
+# chmod 755 timekeeper/reversearray.py
+# chmod 755 timekeeper/selectionsort.py
+# chmod 755 timekeeper/subsequencesum.py
+# chmod 755 timekeeper/testpythoninbuilt.py
+# chmod 755 timekeeper/zeroesandones.py
+# chmod 755 timekeeper/timekeeper.py
+
+#> timekeeper/resultfiles/bubblesort.csv
+#timekeeper/bubblesort.py &&
+> timekeeper/resultfiles/duplicates.csv &&
+timekeeper/duplicates.py &&
+> timekeeper/resultfiles/mechacoach.csv &&
+timekeeper/mechacoach.py &&
+> timekeeper/resultfiles/mostfrequentwords.csv &&
+timekeeper/mostfrequentwords.py &&
+> timekeeper/resultfiles/reversearray.csv &&
+timekeeper/reversearray.py &&
+> timekeeper/resultfiles/selectionsort.csv &&
+timekeeper/selectionsort.py &&
+> timekeeper/resultfiles/subsequencesum.csv &&
+timekeeper/subsequencesum.py &&
+> timekeeper/resultfiles/testpythoninbuilt.csv &&
+timekeeper/testpythoninbuilt.py &&
+> timekeeper/resultfiles/zeroesandones.csv &&
+timekeeper/zeroesandones.py &&
